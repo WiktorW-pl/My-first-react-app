5e30b2c1820abd4cb49776cdea7a0dce67caeeaa
WiktorW-PL <modusii.90@wp.pl>
2022-07-14 14:59:16 +0200

    Reorganizing files structure

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css';
 
@@ -10,6 +10,7 @@ const ExpenseItem = (props) =>{
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button>Change title</button>
         </Card>
     );
 }
