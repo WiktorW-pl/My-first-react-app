7dceafc3cee815f629f1382f9e2309f1e53d6c61
WiktorW-PL <modusii.90@wp.pl>
2022-07-15 22:05:37 +0200

    Add filter display conditions

--- components/Expenses/Expenses.js
+++ components/Expenses/Expenses.js
@@ -10,22 +10,25 @@ const Expenses = (props) =>{
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
-        console.log(selectedYear)
-        // const sfiltrowane = props.items.date;
-        // console.log(props.items.date.lenght)
-        props.items.filter((dupa, index) => {
-            if(selectedYear == dupa[index]) console.log(dupa)
-            else console.log('chuj')
-        })
     }
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if(filteredExpenses.length > 0){
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>
+        ));
+    }
+
     return (
         <div>
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {
-            props.items.map((expense) => (
-                <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}></ExpenseItem>
-            ))}
+           {expensesContent}
         </Card>
         </div>
     )
